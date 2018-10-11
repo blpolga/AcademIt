6522172c2c522ec3561658dcd0d17bb0f0946bf1
Ольга <belopolovaolga@gmail.com>
2018-10-11 20:54:59 +0700

    Сделан сложный Range

--- Range/src/ru/academits/blpolga/range/Range.java
+++ Range/src/ru/academits/blpolga/range/Range.java
@@ -32,4 +32,83 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public void printLength() {
+        System.out.println("Длина диапазона = " + getLength());
+    }
+
+    public static void printArrayUnion(Range[] intervalUnion) {
+        for (Range element : intervalUnion) {
+            System.out.println("Обединение интервалов с " + element.getFrom() + " по " +
+                    element.getTo());
+        }
+    }
+
+    public static void printArrayDifference(Range[] intervalDifference) {
+        if (intervalDifference.length == 0) {
+            System.out.println("Разность равна нулю");
+            return;
+        }
+
+        for (Range element : intervalDifference) {
+            System.out.println("Разность интервалов с " + element.getFrom() + " по " +
+                    element.getTo());
+        }
+    }
+
+    public Range getIntersection(Range range) {
+        if (Math.max(from, range.from) >= Math.min(to, range.to)) {
+            return null;
+        } else {
+            return new Range(Math.max(from, range.from), Math.min(to, range.to));
+        }
+    }
+
+    public Range[] getUnion(Range range) {
+        if (Math.max(from, range.from) > Math.min(to, range.to)) {
+            return new Range[]{
+                    new Range(from, to),
+                    new Range(range.from, range.to)
+            };
+        } else if (range.from <= from && range.to <= to) {
+            return new Range[]{
+                    new Range(range.from, to)
+            };
+        } else if (range.from >= from && range.to <= to) {
+            return new Range[]{
+                    new Range(from, to)
+            };
+        } else {
+            return new Range[]{
+                    new Range(from, range.to)
+            };
+        }
+    }
+
+    public Range[] getDifference(Range range) {
+        if (from <= range.to && range.to <= to && from > range.from) {
+            return new Range[]{
+                    new Range(range.to + 1, to)
+            };
+        } else if (from >= range.from && to <= range.to) {
+            return new Range[]{};
+        } else if (to < range.from || from > range.to) {
+            return new Range[]{
+                    new Range(from, to)
+            };
+        } else if (range.from <= to && to <= range.to) {
+            return new Range[]{
+                    new Range(from, range.from - 1)
+            };
+        } else if (range.from == from && range.to < to) {
+            return new Range[]{
+                    new Range(range.to + 1, to)
+            };
+        } else {
+            return new Range[]{
+                    new Range(from, range.from - 1),
+                    new Range(range.to + 1, to),
+            };
+        }
+    }
 }
--- Range/src/ru/academits/blpolga/range/main/Main.java
+++ Range/src/ru/academits/blpolga/range/main/Main.java
@@ -6,17 +6,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(0, 0);
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите начало диапазона");
-        range.setFrom(scanner.nextDouble());
-        System.out.println("Введите конец диапазона");
-        range.setTo(scanner.nextDouble());
+        System.out.println("Введите начало и конец первого диапазона");
+        double from1 = scanner.nextDouble();
+        double to1 = scanner.nextDouble();
 
-        System.out.println("Начало диапазона = " + range.getFrom());
-        System.out.println("Конец диапазона = " + range.getTo());
-        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.println("Введите начало и конец второго диапазона");
+        double from2 = scanner.nextDouble();
+        double to2 = scanner.nextDouble();
+
+        Range range1 = new Range(from1, to1);
+        Range range2 = new Range(from2, to2);
+
+        range1.printLength();
+        range2.printLength();
+
+        Range range3 = range1.getIntersection(range2);
+        if (range3 == null) {
+            System.out.println("Пересечения нет");
+        } else {
+            System.out.println("Пересечение от " + range3.getFrom() + " до " + range3.getTo());
+        }
+
+        Range[] intervalUnion = range1.getUnion(range2);
+        Range.printArrayUnion(intervalUnion);
+
+        Range[] intervalDifference = range1.getDifference(range2);
+        Range.printArrayDifference(intervalDifference);
+
+        Range range = new Range(0, 0);
+        System.out.println("Задайте границы нового диапазона");
+        range.setFrom(scanner.nextDouble());
+        range.setTo(scanner.nextDouble());
 
         System.out.println("Введите число");
         double number = scanner.nextDouble();
