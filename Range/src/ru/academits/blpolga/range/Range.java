a60da29ddba7d6bdf2fe0ab9b8314ae6533c7450
Ольга <belopolovaolga@gmail.com>
2018-10-12 18:22:07 +0700

    Исправлены ошибки

--- Range/src/ru/academits/blpolga/range/Range.java
+++ Range/src/ru/academits/blpolga/range/Range.java
@@ -39,8 +39,7 @@ public class Range {
 
     public static void printArrayUnion(Range[] intervalUnion) {
         for (Range element : intervalUnion) {
-            System.out.println("Обединение интервалов с " + element.getFrom() + " по " +
-                    element.getTo());
+            System.out.println("Обединение интервалов с " + element.from + " по " + element.to);
         }
     }
 
@@ -51,64 +50,45 @@ public class Range {
         }
 
         for (Range element : intervalDifference) {
-            System.out.println("Разность интервалов с " + element.getFrom() + " по " +
-                    element.getTo());
+            System.out.println("Разность интервалов с " + element.from + " по " + element.to);
         }
     }
 
     public Range getIntersection(Range range) {
-        if (Math.max(from, range.from) >= Math.min(to, range.to)) {
+        double maxFrom = Math.max(from, range.from);
+        double minTo = Math.min(to, range.to);
+        if (maxFrom >= minTo) {
             return null;
-        } else {
-            return new Range(Math.max(from, range.from), Math.min(to, range.to));
         }
+        return new Range(maxFrom, minTo);
     }
 
     public Range[] getUnion(Range range) {
-        if (Math.max(from, range.from) > Math.min(to, range.to)) {
-            return new Range[]{
-                    new Range(from, to),
-                    new Range(range.from, range.to)
-            };
-        } else if (range.from <= from && range.to <= to) {
-            return new Range[]{
-                    new Range(range.from, to)
-            };
-        } else if (range.from >= from && range.to <= to) {
-            return new Range[]{
-                    new Range(from, to)
-            };
+        double maxFrom = Math.max(from, range.from);
+        double maxTo = Math.max(to, range.to);
+        double minFrom = Math.min(from, range.from);
+        double minTo = Math.min(to, range.to);
+
+        if (maxFrom > minTo) {
+            return new Range[]{new Range(minFrom, minTo), new Range(maxFrom, maxTo)};
         } else {
-            return new Range[]{
-                    new Range(from, range.to)
-            };
+            return new Range[]{new Range(minFrom, maxTo)};
         }
     }
 
     public Range[] getDifference(Range range) {
         if (from <= range.to && range.to <= to && from > range.from) {
-            return new Range[]{
-                    new Range(range.to + 1, to)
-            };
+            return new Range[]{new Range(range.to, to)};
         } else if (from >= range.from && to <= range.to) {
             return new Range[]{};
         } else if (to < range.from || from > range.to) {
-            return new Range[]{
-                    new Range(from, to)
-            };
+            return new Range[]{new Range(from, to)};
         } else if (range.from <= to && to <= range.to) {
-            return new Range[]{
-                    new Range(from, range.from - 1)
-            };
+            return new Range[]{new Range(from, range.from)};
         } else if (range.from == from && range.to < to) {
-            return new Range[]{
-                    new Range(range.to + 1, to)
-            };
+            return new Range[]{new Range(range.to, to)};
         } else {
-            return new Range[]{
-                    new Range(from, range.from - 1),
-                    new Range(range.to + 1, to),
-            };
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
         }
     }
 }
